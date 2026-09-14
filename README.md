7d412f336794797dad32084514f749f2ad7ef8d0
ausydhafa <ausyydhafaa@gmail.com>
2026-09-14 17:50:30 +0700

    Tugas 2 - Menambahkan model education

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Nama : Ausy Dhafa Adhitama
 NPM : 2406417954
 
 Kelas : PBP D
+
+### Tugas 2
+
+1. ...
+2. ...
+3. ...
--- main/models.py
+++ main/models.py
@@ -24,3 +24,12 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Education(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    school = models.CharField(max_length=255)
+    year = models.IntegerField()
+    level = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.school
